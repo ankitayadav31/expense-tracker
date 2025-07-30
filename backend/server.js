0edf2f9aad8922f5d14e238988ef513cb2f80343
Ankita Yadav <ankitayadav09239@gmail.com>
2025-07-30 10:00:57 +0530

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes")
-const incomeRoutes = require("./routes/incomeRoutes")
-const expenseRoutes = require("./routes/expenseRoutes")
-const dashboardRoutes = require('./routes/dashboardRoutes')
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
 //Middleware to handle cors
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    headers: ["Content-Type", "Authorization"],
   })
 );
 
@@ -23,15 +23,14 @@ app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {/
   res.send("API is live 👍");
 });
 
-
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/income", incomeRoutes)
-app.use("/api/v1/expense", expenseRoutes)
-app.use("/api/v1/dashboard", dashboardRoutes)
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 //serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
