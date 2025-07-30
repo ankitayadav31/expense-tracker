678faab9e99d2985b6cc3e927934a8df20e13ca7
Ankita Yadav <ankitayadav09239@gmail.com>
2025-07-30 10:35:39 +0530

    fix: image url

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -19,9 +19,7 @@ router.post("/upload-image", upload.single("image"), (req, res) => {
   if(!req.file){
     return res.status(400).json({message: "no file uploaded"});
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
+  const imageUrl = `https://${req.get("host")}/uploads/${req.file.filename}`;
   res.status(200).json({imageUrl});
 });
 
